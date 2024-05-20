dbfbca642b05673e246861541a8f238d0fe0e321
KARAN GUPTA <karan.20gcebcs103@galgotiacollege.edu>
2024-05-20 16:21:07 +0530

    Create promiseAll.js

---
+++ PromiseAll/promiseAll.js
@@ -0,0 +1,17 @@
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 300, "resolved");
+});
+
+const promise2 = 93;
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, "resolved2");
+});
+
+Promise.all([promise1, promise2, promise3])
+.then((values) => {
+    console.log(values);
+})
+.catch((err) => {
+    console.log(err);
+});
